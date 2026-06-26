1bb53efc94c8eb89ad94b09567665964d44a6dbe
saulinluizrocha-prog <saulinluizrocha@gmail.com>
2026-06-26 10:47:27 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -188,7 +188,16 @@
   data-tag="CDrLKRe2qV0MP584y0wNMz" 
   src="https://cdn.cckdl.com/js/base.min.js">
 </script>
-  
+
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-18247302619"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-18247302619');
+</script>
 </head>
 
   <body class="iti-mobile">
